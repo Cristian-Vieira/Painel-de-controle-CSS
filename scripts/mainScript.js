5f72b78be3cb8cd5fac96e9ccd47e39476b2738a
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-22 18:32:51 -0300

    Update version 0.9

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,7 +1,3 @@
-// Seleciona os elementos do DOM que serão utilizados
-const openModalBtn = document.getElementById('openModal');
-const modal = document.getElementById('modal');
-const closeModalBtn = document.getElementById('closeModal');
 const applyChangesBtn = document.getElementById('applyChanges');
 const controlPanelForm = document.getElementById('controlPanel');
 const bgColorInput = document.getElementById('bgColor');
@@ -9,15 +5,8 @@ const textColorInput = document.getElementById('textColor');
 const fontSizeInput = document.getElementById('fontSize');
 const fontFamilyInput = document.getElementById('fontFamily');
 
-// Define a função que será executada ao clicar no botão "Abrir Painel de Controle"
-function openModal() {
-    modal.style.display = 'block';
-}
-
-// Define a função que será executada ao clicar no botão "Fechar"
-function closeModal() {
-    modal.style.display = 'none';
-}
+// div de prévia
+const previewDiv = document.getElementById("preview");
 
 // Define a função que será executada ao clicar no botão "Aplicar"
 function applyChanges() {
@@ -29,17 +18,13 @@ function applyChanges() {
 }
 
 // Adiciona os event listeners
-openModalBtn.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', closeModal);
 applyChangesBtn.addEventListener('click', applyChanges);
 controlPanelForm.addEventListener('submit', event => event.preventDefault());
 
 
+previewDiv.style.backgroundColor = bgColorInput.value;
+previewDiv.style.color = textColorInput.value;
 // selecione os elementos do painel de controle
-
-// selecione a div de prévia
-const previewDiv = document.getElementById("preview");
-
 // adicione um evento "change" a cada elemento de entrada do painel de controle
 bgColorInput.addEventListener("change", () => {
     previewDiv.style.backgroundColor = bgColorInput.value;
@@ -56,3 +41,4 @@ fontSizeInput.addEventListener("change", () => {
 fontFamilyInput.addEventListener("change", () => {
     previewDiv.style.fontFamily = fontFamilyInput.value;
 });
+
