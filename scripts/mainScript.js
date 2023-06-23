cad06ac990880f1c907d42c84a573c161e35acb4
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-23 17:24:36 -0300

    Update mainScript.js
    Refactoring

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,44 +1,44 @@
-const applyChangesBtn = document.getElementById('applyChanges');
-const controlPanelForm = document.getElementById('controlPanel');
-const bgColorInput = document.getElementById('bgColor');
-const textColorInput = document.getElementById('textColor');
-const fontSizeInput = document.getElementById('fontSize');
-const fontFamilyInput = document.getElementById('fontFamily');
+// Get elementos do DOM
+const APLICAR_BTN = document.getElementById('applyChanges');
+const CONTROLPAINEL_FORM = document.getElementById('controlPanel');
+const BGCOLOR_INPUT = document.getElementById('bgColor');
+const TEXTCOLOR_INPUT = document.getElementById('textColor');
+const FONTSIZE_INPUT = document.getElementById('fontSize');
+const FONTFAMILY_INPUT = document.getElementById('fontFamily');
+const PREVIEW_DIV = document.getElementById("preview");
 
-// div de prévia
-const previewDiv = document.getElementById("preview");
+ // Padrizando Style dos elemetos de preview das alterações 
+PREVIEW_DIV.style.backgroundColor = BGCOLOR_INPUT.value;
+PREVIEW_DIV.style.color = TEXTCOLOR_INPUT.value;
 
-// Define a função que será executada ao clicar no botão "Aplicar"
+ // Função do botão "Aplicar"
 function applyChanges() {
-    document.body.style.backgroundColor = bgColorInput.value;
-    document.body.style.color = textColorInput.value;
-    document.body.style.fontSize = `${fontSizeInput.value}px`;
-    document.body.style.fontFamily = fontFamilyInput.value;
+    document.body.style.backgroundColor = BGCOLOR_INPUT.value;
+    document.body.style.color = TEXTCOLOR_INPUT.value;
+    document.body.style.fontSize = `${FONTSIZE_INPUT.value}px`;
+    document.body.style.fontFamily = FONTFAMILY_INPUT.value;
     closeModal();
 }
 
-// Adiciona os event listeners
-applyChangesBtn.addEventListener('click', applyChanges);
-controlPanelForm.addEventListener('submit', event => event.preventDefault());
+ // Event listeners para aplicar alterações 
+APLICAR_BTN.addEventListener('click', applyChanges);
+CONTROLPAINEL_FORM.addEventListener('submit', event => event.preventDefault());
 
 
-previewDiv.style.backgroundColor = bgColorInput.value;
-previewDiv.style.color = textColorInput.value;
-
-// adicione um evento "change" a cada elemento de entrada do painel de controle
-bgColorInput.addEventListener("change", () => {
-    previewDiv.style.backgroundColor = bgColorInput.value;
+ // Trocar style da div de preview ao alterar os valores dos inputs
+BGCOLOR_INPUT.addEventListener("change", () => {
+    PREVIEW_DIV.style.backgroundColor = BGCOLOR_INPUT.value;
 });
 
-textColorInput.addEventListener("change", () => {
-    previewDiv.style.color = textColorInput.value;
+TEXTCOLOR_INPUT.addEventListener("change", () => {
+    PREVIEW_DIV.style.color = TEXTCOLOR_INPUT.value;
 });
 
-fontSizeInput.addEventListener("change", () => {
-    previewDiv.style.fontSize = fontSizeInput.value + "px";
+FONTSIZE_INPUT.addEventListener("change", () => {
+    PREVIEW_DIV.style.fontSize = FONTSIZE_INPUT.value + "px";
 });
 
-fontFamilyInput.addEventListener("change", () => {
-    previewDiv.style.fontFamily = fontFamilyInput.value;
+FONTFAMILY_INPUT.addEventListener("change", () => {
+    PREVIEW_DIV.style.fontFamily = FONTFAMILY_INPUT.value;
 });
 
